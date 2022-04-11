64b35764124f3f99bc47ae4da94c6d06608ed2de
WangKuoKuo <541732105@qq.com>
2022-04-11 21:31:27 +0800

    第三次提交

--- src/test/java/com.kgc.git/GitTest.java
+++ src/test/java/com.kgc.git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("Hello Git");
         System.out.println("Hello Git2");
         System.out.println("Hello Git3");
+        System.out.println("Hello Git4");
     }
 
 }
